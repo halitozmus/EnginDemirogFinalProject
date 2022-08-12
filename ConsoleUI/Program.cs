8553033e8d25c77332ee980326e0f086f1ab00d4
Halit Özmüş <halit.ozmuss@gmail.com>
2022-08-12 14:07:26 +0300

    12.08.2022-14:06 EnginDemirogFinalProject

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,8 +13,18 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetAllByUnitPrice(50,100))
+            foreach (var item in productManager.GetAllByUnitPrice(50, 100))
             {
                 Console.WriteLine(item.ProductName);
             }
--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -8,14 +8,15 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class NorthwindContext:DbContext
+    public class NorthwindContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
         }
-        public DbSet<Product> Products{ get; set; }
-        public DbSet<Category> Categories{ get; set; }
-        public DbSet<Customer> Customers{ get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders{ get; set; }
     }
 }
