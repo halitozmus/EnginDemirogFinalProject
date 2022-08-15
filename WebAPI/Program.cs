7ae1a5e3a08ef8c4db65a4bff895e503a1771f96
Halit Özmüş <halit.ozmuss@gmail.com>
2022-08-15 16:38:50 +0300

    15.08.2022 16:38

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,22 +1,32 @@
+using Autofac;
+using Business.DependencyResolvers.Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder=>
+{
+    builder.RegisterModule(new AutofacBusinessModule());
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 //Autofac,Ninject,CastleWindsor,StructureMap,LightInject,DryInject--->IoC Container
 //AOP Bütün metotlarda iþlem yapmak(Exp Log) istersen kullan.Exp[LogAspect][Validate][Cache][Transaction]
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal,EfProductDal>();
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal,EfProductDal>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,7 +34,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
